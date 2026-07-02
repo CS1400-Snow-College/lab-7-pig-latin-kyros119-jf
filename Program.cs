9c50714c0bfe289c4e4b4095bcc4e4e0e39a644f
Jeremy <jeremy.frank@students.snow.edu>
2026-07-02 12:28:52 -0600

    add header and greeting

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Programmer: Jeremy Frank
+// Date: July 1, 2026
+// Lab 7 - Pig Latin 
+
+
+Console.WriteLine("Let's switch-up your phrase!");
+Console.WriteLine("This program will turn your message into Pig Latin and then encrypt it.");
+Console.WriteLine();
